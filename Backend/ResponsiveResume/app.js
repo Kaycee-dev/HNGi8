b788d3cdce9b6cdb77bdf1cbcd1166665a555a0b
Uba Kelechi Michael <kcsplace20@gmail.com>
2021-08-20 17:29:56 +0100

    Original Commit

---
+++ Backend/ResponsiveResume/app.js
@@ -0,0 +1,15 @@
+// const sign_in_btn = document.querySelector(".panel.right-panel");
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+// const resume = document.querySelector("#resume");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+  $("#resume").hide();
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+  $("#resume").show();
+});
